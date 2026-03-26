1611f3eca01f200df496ae96b962cd1fa84ebe4c
BoredWasRobbed <bored7160@gmail.com>
2026-03-26 03:44:10 -0400

    Updated ReplicatedStorage/Modules/Assets.lua

---
+++ ReplicatedStorage/Modules/Assets.lua
@@ -0,0 +1,23 @@
+-- @ScriptType: ModuleScript
+local Assets = {
+	PlayerAnimations = {
+		WalkLeft = "rbxassetid://70683079295931",
+		WalkRight = "rbxassetid://111157563600777",
+		WalkForward = "rbxassetid://112297888762803",
+		WalkBackward = "rbxassetid://78791079117962",
+		Idle = "rbxassetid://105241454561702",
+		Sprint = "rbxassetid://132892122918560",
+		WallIdle = "rbxassetid://86982327404805",
+	},
+	
+	StandAnimations = {
+		BasicStandSummon = "rbxassetid://132173979661212",
+		BasicStandIdle = "rbxassetid://117243129930131",
+	},
+	
+	Sounds = {
+		
+	}
+}
+
+return Assets
