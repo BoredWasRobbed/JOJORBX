4d21f66e5645e93288b8b9542339a727a3aabcec
BoredWasRobbed <bored7160@gmail.com>
2026-03-30 18:55:24 -0400

    Updated ReplicatedStorage/Modules/UIHandler.lua

--- ReplicatedStorage/Modules/UIHandler.lua
+++ ReplicatedStorage/Modules/UIHandler.lua
@@ -1,30 +1,28 @@
 -- @ScriptType: ModuleScript
 local UIHandler = {}
 
-local UIelements = {}
+local UIElements = {}
 
 function UIHandler._init(PlayerGui: PlayerGui)
 	local PlayerUI = PlayerGui:WaitForChild("PlayerUi")
 	local LevelFrame = PlayerUI:WaitForChild("LevelBar")
 
-	UIelements = {
+	UIElements = {
 		Level = LevelFrame:WaitForChild("LevelText"),	
 	}
 	
-	print(UIelements)
+	print(UIElements)
 end
 
 function UIHandler.UpdateText(GuiName: string, Value: number, Format: string)
-	local UI = UIelements[GuiName]
+	local UI = UIElements[GuiName]
 	
 	if UI then
-	UI.Text = Format:format(Value)
+		UI.Text = Format:format(Value)
 	else
 		warn("Ui aint found big dawg")
 	end
 end
-	
-
 
 return UIHandler
 
