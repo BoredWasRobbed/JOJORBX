a897c6670652f5fcc03671bcc020f843b6487364
BoredWasRobbed <bored7160@gmail.com>
2026-04-03 06:02:11 -0400

    Updated ReplicatedStorage/Modules/Assets.lua

--- ReplicatedStorage/Modules/Assets.lua
+++ ReplicatedStorage/Modules/Assets.lua
@@ -7,12 +7,36 @@ local Assets = {
 		WalkBackward = "rbxassetid://78791079117962",
 		Idle = "rbxassetid://105241454561702",
 		Sprint = "rbxassetid://132892122918560",
-		WallIdle = "rbxassetid://86982327404805",
+
+		HitReaction1 = "rbxassetid://88331526174051",
+		HitReaction2 = "rbxassetid://110330970797998",
+		HitReaction3 = "rbxassetid://100570673693551",
+		HitReaction4 = "rbxassetid://108344734088084",
+
+		BlockReaction1 = "rbxassetid://111539832537526",
+		BlockReaction2 = "rbxassetid://132771215270046",
+		BlockReaction3 = "rbxassetid://122741029485299",
+		BlockReaction4 = "rbxassetid://115869946222437",
+
+		DashForward = "rbxassetid://83278988155808",
+		DashBack = "rbxassetid://77091455342842",
+		DashLeft = "rbxassetid://133806017839033",
+		DashRight = "rbxassetid://107964968746967",
+
+		Block = "rbxassetid://123740957953259",
+		Parry = "rbxassetid://91908466669667",
+		Parried = "rbxassetid://109231501978287",
 	},
 	
 	StandAnimations = {
 		BasicStandSummon = "rbxassetid://126510194598654",
 		BasicStandIdle = "rbxassetid://105343434029961",
+		
+		Punch1 = "rbxassetid://91736475510924",
+		Punch2 = "rbxassetid://78700173295657",
+		Punch3 = "rbxassetid://100253962623297",
+		Punch4 = "rbxassetid://128495273135460",
+		HeavyAttack = "rbxassetid://132032561561025",
 	},
 	
 	Sounds = {
