3b03179516051c691d3871a0cb45ef8d162213ef
BoredWasRobbed <bored7160@gmail.com>
2026-04-03 07:06:27 -0400

    Updated ReplicatedStorage/Modules/StateMachine/Blocking.lua

--- ReplicatedStorage/Modules/StateMachine/Blocking.lua
+++ ReplicatedStorage/Modules/StateMachine/Blocking.lua
@@ -4,24 +4,22 @@ Blocking.__index = Blocking
 
 function Blocking.new(machine)
 	local self = setmetatable({}, Blocking)
-
 	self.Name = "Blocking"
 	self.Category = "Defense"
 	self.Time = 0
-
 	self.Machine = machine
 	return self
 end
 
 function Blocking:CanEnter()
-	return not self.Machine:HasState("Stunned") and not self.Machine:HasState("Attack")
+	return not self.Machine:HasState("Stunned") and not self.Machine:HasState("Attacking")
 end
 
 function Blocking:OnEnter()
 	local humanoid = self.Machine.Character:FindFirstChild("Humanoid")
 	if humanoid then
 		self.OldWalkSpeed = humanoid.WalkSpeed
-		humanoid.WalkSpeed = 4 -- Slow movement while blocking
+		humanoid.WalkSpeed = 4
 	end
 end
 
